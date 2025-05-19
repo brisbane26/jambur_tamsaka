9d60895e50289ef43046b5e22ffc52708a79089f
petrr4 <petraik111@gmail.com>
2025-05-19 22:55:59 +0700

    seeder paket

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Kategori;
+use App\Models\Bank;
+use App\Models\Paket;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -37,6 +39,7 @@ class DatabaseSeeder extends Seeder
         $this->call(AdminUserSeeder::class);
         $this->call(KategoriSeeder::class);
         $this->call(BankSeeder::class);
+        $this->call(PaketSeeder::class);
         // $this->call(PermissionsSeeder::class);
     }
 
